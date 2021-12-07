41535552691541ef3f6dad2b0d77cb38ffbb2b4b
Ari Razab <arianrazab@yahoo.com>
2021-12-08 00:32:33 +0100

    Initial commit

---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but i don\'t think you\'ll like it')
+    
+    
+def square_root(x):    
+    return math.sqrt(x)
